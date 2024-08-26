5337e0ffda7375b6688c1ae1796077dbeb31b698
MuhammadShahziab <muhammadshahzaib138@gmail.com>
2024-08-27 03:32:25 +0500

    dbConnection

--- app/db/Connect.js
+++ app/db/Connect.js
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const ConnectDB = async () => {
-  if (mongoose.connection.readyState === 0) {
-    try {
-      await mongoose.connect(process.env.MONGO_URL);
-      console.log("CONNECTED TO MONGODB SUCCESSFULLY");
-    } catch (error) {
-      console.error("Connection to MongoDB failed:", error.message);
-      process.exit(1);
-    }
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already Connected");
+  }
+
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+
+    isConnected = true;
+
+    console.log("MongoDB is connected!");
+  } catch (error) {
+    console.log(error);
   }
 };
 
